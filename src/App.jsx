dc5f0a82e93e7113afa91dde5b7b5bd23539cbd7
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2024-06-28 14:51:08 -0300

    feat: add all components to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
 import "./App.css";
+import Footer from "./components/footer";
+import React from "react";
+
+import Hero from "./components/hero";
+import Course from "./components/course";
+import CourseCreator from "./components/course-creator";
 
 function App() {
   return (
-    <div className="App">
-      <p>Project Hackaton</p>
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <Hero />
+      <Course />
+      <CourseCreator />
+      <div className="flex-grow"></div>
+      <Footer />
     </div>
   );
 }
