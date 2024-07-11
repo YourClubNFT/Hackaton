9e441575b66eb1ab9cae15c44a4052d4401746f4
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2024-07-11 19:59:55 -0300

    feat: add method for access-control and price definition

--- src/components/course.jsx
+++ src/components/course.jsx
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import courseImage from "../assets/images/course_image.png";
-import { getContract } from "viem";
 import wagmiAbi from "./abi.json";
 import { celoAlfajores } from "viem/chains";
 import { account, publicClient, walletClient } from "./config";
+import { BallTriangle } from "react-loader-spinner";
+import ModalChangePrice from "./modal-change-price";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Course() {
+  const [hasAccess, setHasAccess] = useState(false);
+  const [isOwner, setIsOwner] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [checkingAccess, setCheckingAccess] = useState(true);
+
+  useEffect(() => {
+    checkAccess();
+  }, []);
+
+  // Access-Control
+  const checkAccess = async () => {
+    setCheckingAccess(true);
+    try {
+      if (!publicClient) {
+        throw new Error("Public client is not initialized.");
+      }
+
+      const balance = await publicClient.readContract({
+        account,
+        address: "0x66bbc9D1E831A4783C6B5e1B107C4Ce8CA05F7fc",
+        abi: wagmiAbi,
+        functionName: "balanceOf",
+        args: [account],
+      });
+
+      setHasAccess(balance > 0);
+
+      const owner = await publicClient.readContract({
+        account,
+        address: "0x66bbc9D1E831A4783C6B5e1B107C4Ce8CA05F7fc",
+        abi: wagmiAbi,
+        functionName: "owner",
+      });
+
+      setIsOwner(owner.toLowerCase() === account.toLowerCase());
+    } catch (error) {
+      console.error("Error checking access:", error);
+    } finally {
+      setCheckingAccess(false);
+    }
+  };
+
   const handleButtonClick = async () => {
+    setLoading(true);
     try {
       if (!walletClient) {
         throw new Error("Wallet client is not initialized.");
@@ -34,11 +79,63 @@ export default function Course() {
     } catch (error) {
       console.error("Error during contract interaction:", error);
       alert("Error during contract interaction: " + error.message);
+    } finally {
+      setLoading(false);
+      checkAccess();
     }
+  };
+
+  const handleAccessCourse = () => {
+    if (hasAccess) {
+      alert("Access granted. Navigate to the course content.");
+    } else {
+      toast.error("You need to buy this course first.", {
+        duration: 2000,
+        style: {
+          border: "1px solid #010D7E",
+          padding: "16px",
+          color: "#010D7E",
+          background: "#f3f4f6",
+        },
+        iconTheme: {
+          primary: "#FF0000",
+          secondary: "#FFFAEE",
+        },
+        position: "top-right",
+        icon: "⚠️",
+        className: "text-sm",
+      });
+    }
+  };
+
+  const savePrice = (newPrice) => {
+    return new Promise((resolve, reject) => {
+      //TODO: Lógica para salvar o preço no cintrato
+      setTimeout(() => {
+        if (newPrice) {
+          resolve("Price saved successfully.");
+        } else {
+          reject("Failed to save price.");
+        }
+      }, 2000);
+    });
+  };
+
+  const handleSaveNewPrice = (newPrice) => {
+    toast
+      .promise(savePrice(newPrice), {
+        loading: "Saving...",
+        success: <b>Price saved!</b>,
+        error: <b>Could not save.</b>,
+      })
+      .then(() => {
+        document.getElementById("change-price").close();
+      });
   };
 
   return (
     <section className="p-6 bg-gray-100 text-gray-800">
+      <Toaster />
       <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
         <img
           src={courseImage}
@@ -59,17 +156,67 @@ export default function Course() {
             insights to excel in the ever-evolving business world.
           </p>
 
-          <button
-            className="btn btn-outline w-full border-[#010D7E] text-[#010D7E] hover:bg-[#010D7E] hover:text-white"
-            onClick={handleButtonClick}
-          >
-            Mint (Buy Course)
-          </button>
+          <div className="flex flex-row items-center justify-between mb-4">
+            {checkingAccess ? (
+              <button
+                className="btn btn-outline w-[45%] border-[#010D7E] text-[#010D7E] disabled:text-gray-400  disabled:opacity-50"
+                disabled
+              >
+                <BallTriangle
+                  height="24"
+                  width="24"
+                  color="#9ca3af"
+                  ariaLabel="loading"
+                />
+              </button>
+            ) : isOwner ? (
+              <button
+                className="btn btn-outline w-[45%] border-[#010D7E] text-[#010D7E] hover:bg-[#010D7E] hover:text-white"
+                onClick={() =>
+                  document.getElementById("change-price").showModal()
+                }
+              >
+                Change Price
+              </button>
+            ) : (
+              <button
+                className={`btn w-[45%] border-[#010D7E] ${
+                  hasAccess
+                    ? "bg-gray-500 text-gray-400 cursor-not-allowed"
+                    : "btn-outline text-[#010D7E] hover:bg-[#010D7E] hover:text-white"
+                }`}
+                onClick={handleButtonClick}
+                disabled={loading}
+              >
+                {loading ? (
+                  <BallTriangle
+                    height="24"
+                    width="24"
+                    color="#9ca3af"
+                    ariaLabel="loading"
+                  />
+                ) : (
+                  "Mint (Buy Course)"
+                )}
+              </button>
+            )}
+            <button
+              className={`btn w-[45%]  ${
+                hasAccess
+                  ? "btn-outline text-[#010D7E] border-[#010D7E] hover:bg-[#010D7E] hover:text-white"
+                  : "bg-gray-500 text-gray-400 border-gray-500 cursor-not-allowed"
+              }`}
+              onClick={handleAccessCourse}
+            >
+              Access Course
+            </button>
+          </div>
           <p className="mt-4 text-sm text-gray-500">
             You need to be in Celo Minipay to mint the course.
           </p>
         </div>
       </div>
+      <ModalChangePrice onSave={handleSaveNewPrice} />
     </section>
   );
 }
